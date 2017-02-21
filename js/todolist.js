1d1c539e905993087b4c9663bdbfe9f852b3e5f8
David Lindley <david.lindley@gmail.com>
2017-02-21 11:30:11 +0000

    Added todo list.js

---
+++ js/todolist.js
@@ -0,0 +1,31 @@
+/*
+* This is getting the element with the ID add-btn. It is then adding an event listener to it
+* when a "click" is detected it will run the addBtnPressed function
+*/
+document.getElementById("add-btn").addEventListener("click", addBtnPressed);
+
+
+/*
+* Runs when the add button is pressed.
+*/
+function addBtnPressed(event) {
+  /*
+  * TODO: Get the value from the text field (document.getELementById)
+  * Pass the value to addItemToList()
+  * Clear the text field so it is ready for the next entry
+  */
+}
+
+/*
+* Adds newItem to the list
+*/
+function addItemToList(newItem) {
+  /*
+  * TODO: Create a string which contains HTML for a new li element
+  * put the new item inside the li element
+  * The li element should have the structure: '<li class="list-group-item">list item name</li>'
+  * Add the new li element to the UL element using
+  *   insertAdjacentHTML('beforeend', newItem);
+  */
+
+}
